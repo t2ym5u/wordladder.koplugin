aae368d5077035e3f240d4bd13d22d25ba7387bb
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-21 16:28:38 +0200

    chore: bump version to 1.0.3

--- _meta.lua
+++ _meta.lua
@@ -2,7 +2,7 @@ local _ = require("gettext")
 
 return {
     name        = "wordladder",
-    version     = "1.0.2",
+    version     = "1.0.3",
     fullname    = _("Word Ladder"),
     description = _("Change one word into another, one letter at a time."),
 }
